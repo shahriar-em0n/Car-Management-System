609b50dfa415357f1856a1af21ec5cf21c90c117
shahriar-em0n <contactshahriar12@gmail.com>
2025-11-01 18:40:13 +0600

    Add models engine and car

--- models/engine.go
+++ models/engine.go
@@ -1,1 +1,10 @@
 package models
+
+import "github.com/google/uuid"
+
+type Engine struct {
+	ID             uuid.UUID `json:"engine_id"`
+	Displacement   int64     `json:"displacement"`
+	NoOfCyclinders int64     `json:"no_of_cyclinders"`
+	CarRange       int64     `json:"car_range"`
+}
