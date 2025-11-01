fce50f4e90eb690da163025445faa08c28487a04
shahriar-em0n <contactshahriar12@gmail.com>
2025-11-01 19:15:00 +0600

    add the engine and engine req validation

--- models/engine.go
+++ models/engine.go
@@ -1,10 +1,53 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
 
 type Engine struct {
-	ID             uuid.UUID `json:"engine_id"`
+	EngineID       uuid.UUID `json:"engine_id"`
 	Displacement   int64     `json:"displacement"`
 	NoOfCyclinders int64     `json:"no_of_cyclinders"`
 	CarRange       int64     `json:"car_range"`
+}
+type EngineRequest struct {
+	Displacement   int64     `json:"displacement"`
+	NoOfCyclinders int64     `json:"no_of_cyclinders"`
+	CarRange       int64     `json:"car_range"`
+}
+
+func ValidateEngineRequest(EngineReq EngineRequest ) error{
+	if err:= validateDisplacement(EngineReq.Displacement); err!= nil {
+		return err
+	}
+	if err:= validateNoOfCyclinders(EngineReq.NoOfCyclinders); err!= nil {
+		return err
+	}
+	if err:= validateCarRange(EngineReq.CarRange); err!= nil {
+		return err
+	}
+	return nil
+}
+
+func validateDisplacement(displacement int64) error {
+	if displacement <= 0 {
+		return errors.New("displacement must be greater than zero")
+	}
+	return nil
+}
+
+func validateNoOfCyclinders(NoOfCyclinders int64) error {
+	if NoOfCyclinders <= 0 {
+		return errors.New("Number of Cyclinders must be greater than zero")
+	}
+	return nil
+}
+
+func validateCarRange(CarRange int64) error {
+	if CarRange <= 0 {
+		return errors.New("carRange must be greater than zero")
+	}
+	return nil
 }
