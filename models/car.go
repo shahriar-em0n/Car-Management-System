41c1f5f2299613eafe273f00fdf10d12fc8eec6b
shahriar-em0n <contactshahriar12@gmail.com>
2025-11-01 18:45:40 +0600

    add the validaton of namee and car request option

--- models/car.go
+++ models/car.go
@@ -1,8 +1,10 @@
 package models
 
 import (
+	"errors"
 	"time"
 
+	// "github.com/docker/docker/daemon/names"
 	"github.com/google/uuid"
 )
 
@@ -17,3 +19,20 @@ type Car struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type CarRequest struct{
+	Name string `json:"name"`
+	Year string `json:"year"`
+	Brand string `json:"brand"`
+	FuelType string `json:"fuel_type"`
+	Engine Engine `json:"engine"`
+	Price float64 `json:"price"`
+}
+
+func validateName(name string) error{
+	if name == "" {
+		return errors.New("Name is Required")
+	}
+	return nil
+}
+
