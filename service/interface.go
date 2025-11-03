83e58603a2cb092fcae06902d38cc82b38631594
shahriar-em0n <contactshahriar12@gmail.com>
2025-11-03 18:49:50 +0600

    Add carservice interface

--- service/interface.go
+++ service/interface.go
@@ -1,1 +1,18 @@
 package service
+
+import (
+	"CMS/models"
+	"context"
+)
+
+type CarServiceInterface interface {
+	GetCarByID(ctx context.Context, id string) (*models.Car, error)
+
+	GetCarsByBrand(ctx context.Context, brand string, isEngine bool) ([]models.Car, error)
+
+	CreateCar(ctx context.Context, car *models.CarRequest) (*models.Car, error)
+
+	UpdateCar(ctx context.Context, id string, carReq *models.CarRequest) (*models.Car, error)
+
+	DeleteCar(ctx context.Context, id string) (*models.Car, error)
+}
