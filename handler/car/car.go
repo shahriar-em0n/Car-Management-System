a5b9252710046d45853983b0ecd1221ea2a85ae8
shahriar-em0n <contactshahriar12@gmail.com>
2025-11-03 18:50:19 +0600

    Add newCarHandler function

--- handler/car/car.go
+++ handler/car/car.go
@@ -1,1 +1,14 @@
 package car
+
+import "CMS/service"
+
+
+type CarHandler struct{
+	service service.CarServiceInterface
+}
+
+func NewCarHandler(service service.CarServiceInterface) *CarHandler{
+	return &CarHandler{
+		service: service,
+	}
+}
