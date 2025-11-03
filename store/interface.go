d2953cce90b1a99d579a4e8790a2cddbee74d0dd
shahriar-em0n <contactshahriar12@gmail.com>
2025-11-03 14:51:16 +0600

    Add the interface codebase

--- store/interface.go
+++ store/interface.go
@@ -1,1 +1,19 @@
 package store
+
+import (
+	"CMS/models"
+	"context"
+)
+
+
+type CarStoreInferface interface{
+	GetCarById(ctx context.Context, id string) (models.Car, error)
+
+	GetCarByBrand(ctx context.Context, brand string, isEngine bool) ([]models.Car, error)
+
+	CreateCar(ctx context.Context, carReq *models.CarRequest) (models.Car, error)
+
+	UpdateCar(ctx context.Context, id string, carReq *models.CarRequest) (models.Car, error)
+
+	DeleteCar(ctx context.Context, id string) (models.Car, error)
+}
