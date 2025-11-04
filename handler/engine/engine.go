7f36cd4d520b5fa859d90aaef9346683768426c4
shahriar-em0n <contactshahriar12@gmail.com>
2025-11-04 19:54:41 +0600

    Add Engine by id functionality

--- handler/engine/engine.go
+++ handler/engine/engine.go
@@ -1,1 +1,48 @@
 package engine
+
+import (
+	"CMS/service"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type EngineHandler struct {
+	service service.EngineServiceInterface
+}
+
+func NewEngineHandler(service service.EngineServiceInterface) *EngineHandler {
+	return &EngineHandler{
+		service: service,
+	}
+}
+
+func (e *EngineHandler) GetEngineById(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	resp, err := e.service.GetEngineById(ctx, id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	body, err := json.Marshal(resp)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	_, err = w.Write(body)
+	if err != nil {
+		log.Println("Error Writing Response: ", err)
+	}
+}
