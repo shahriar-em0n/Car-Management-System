041f8e218a1de35f9dc54e366f5803c87a7cb168
shahriar-em0n <contactshahriar12@gmail.com>
2025-11-05 00:55:18 +0600

    Add db parameters

--- driver/postgress.go
+++ driver/postgress.go
@@ -1,1 +1,16 @@
 package driver
+
+import (
+	"fmt"
+	"os"
+)
+
+func InitDB() {
+	connStr := fmt.Sprint("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_POET"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+}
