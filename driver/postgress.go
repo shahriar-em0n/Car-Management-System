9e0aacb0bc764491355b8d976eff1c0020f4c906
shahriar-em0n <contactshahriar12@gmail.com>
2025-11-05 01:08:05 +0600

    init the db

--- driver/postgress.go
+++ driver/postgress.go
@@ -1,16 +1,32 @@
 package driver
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"os"
 )
 
 func InitDB() {
-	connStr := fmt.Sprint("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
-		os.Getenv("DB_POET"),
+		os.Getenv("DB_PORT"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 	)
+
+	db, err := sql.Open("postgress", connStr)
+	if err != nil {
+		log.Fatalf("Error Opeing database: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error Connection to the database: %v", err)
+	}
+
+	fmt.Println("SuccesFully Connected to the database")
 }
+
+
