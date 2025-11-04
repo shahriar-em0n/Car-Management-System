20bfd06120dc22b2eadf47c490af641fd8385333
shahriar-em0n <contactshahriar12@gmail.com>
2025-11-04 20:09:29 +0600

    Add create Engine functionality

--- handler/engine/engine.go
+++ handler/engine/engine.go
@@ -1,8 +1,10 @@
 package engine
 
 import (
+	"CMS/models"
 	"CMS/service"
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 
@@ -45,4 +47,43 @@ func (e *EngineHandler) GetEngineById(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println("Error Writing Response: ", err)
 	}
+}
+
+func(e *EngineHandler) CreateEngine(w http.ResponseWriter, r *http.Request){
+	ctx := r.Context()
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println("Error reading request body:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var engineReq models.EngineRequest
+
+	err = json.Unmarshal(body, &engineReq)
+	if err !=nil {
+		log.Println("Error Unmarshelling engine Request body: ", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	createdEgnine, err := e.service.CreateEngine(ctx, &engineReq)
+	if err != nil {
+		log.Println("Error While Creating Engine: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	resBody, err := json.Marshal(createdEgnine)
+	if err != nil {
+		log.Println("Error marshalling response body: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	_,_ =w.Write(resBody)
 }
